2cb670e59a21194f992419b7ae3008b8676ba54a
TheShip1948 <beshoyatwork@hotmail.com>
2026-03-20 15:39:47 +0100

    Add test3 tutorial

---
+++ ECE408_CNN_Forward_Pass_Project/MiniDNN/tutorials/test3/build.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:: Set build directory
+set BUILD_DIR=build
+set EXECUTABLE=test3.exe
+
+:: Create the build directory if it doesn't exist
+if not exist %BUILD_DIR% (
+    mkdir %BUILD_DIR%
+)
+
+:: Locate Visual Studio to load environment variables
+echo Found Visual Studio, initializing environment...
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+:: Attempt to compile the code
+echo Compiling test3.cpp...
+nvcc -O2 -I..\..\eigen-3.4.0 -I..\..\include -allow-unsupported-compiler .\test3.cpp -o %BUILD_DIR%\%EXECUTABLE%
+
+:: Check if the compilation succeeded
+if %errorlevel% equ 0 (
+    echo.
+    echo Compilation Successful!
+    echo Executable is located at: %BUILD_DIR%\%EXECUTABLE%
+    echo Running the executable...
+    %BUILD_DIR%\%EXECUTABLE%
+) else (
+    echo.
+    echo Compilation Failed!
+)
